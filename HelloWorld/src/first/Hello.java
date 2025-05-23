59ff2c1138f7190b6765969e12b96f59af180e91
dir-FY25igarihana <hana.igari.d865@dirbato.co.jp>
2025-05-23 12:08:58 +0900

    初回

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -3,7 +3,7 @@ package first;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");dir-FY25igarihanadir-FY25igarihana
 
     }
 
